ee3d561aaeaa7e8f27863522144ed327d19ef1d4
(Ev)Angelos <evangelosas@yahoo.com>
2025-09-16 18:29:29 +0300

    Junie 4th iteration - new package

--- main.go
+++ main.go
@@ -4,26 +4,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	gwi "platform-go-challenge/gwiTest"
 )
 
 func main() {
-	var store Store
+	var store gwi.Store
 	if os.Getenv("FAV_STORE") == "file" {
 		path := os.Getenv("FAV_PATH")
 		if path == "" {
 			path = "data/favourites.json"
 		}
-		fs, err := NewFileStore(path)
+		fs, err := gwi.NewFileStore(path)
 		if err != nil {
 			log.Fatalf("failed to init file store: %v", err)
 		}
 		store = fs
 		log.Printf("Using file store at %s", path)
 	} else {
-		store = NewInMemoryStore()
+		store = gwi.NewInMemoryStore()
 		log.Printf("Using in-memory store")
 	}
-	server := NewServer(store)
+	server := gwi.NewServer(store)
 
 	addr := ":8080"
 	log.Printf("Starting favourites service on %s", addr)
